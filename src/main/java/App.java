850da392ebd2910e2dc7225bb045e6c28477bac0
Puchek32 <puchek11@yandex.ru>
2021-11-25 14:58:27 +0300

    complete task

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean = applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+        HelloWorld bean2 = applicationContext.getBean("helloworld", HelloWorld.class);
+        System.out.println(bean.getMessage());
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat.getVoice());
+        Cat cat2 = applicationContext.getBean("cat", Cat.class);
+        System.out.println(cat2.getVoice());
+
+        System.out.println(bean == bean2);
+        System.out.println(cat == cat2);
     }
 }
